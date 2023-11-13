861d4b8bd0bea60b0cb54509a1ceb34269db3842
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2023-11-13 17:54:17 +0100

    i've create the file .gitignore and i've changed some name in the gile TestIterablePlain.java because i've create one class with a different name

--- 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import static it.unibo.inner.test.Assertions.assertContentEqualsInOrder;
 
+import it.unibo.inner.api.IterableWithPolicyImplementation;
+
 public class TestIterablePlain {
 
     private TestIterablePlain() {}
 
     private static <T> IterableWithPolicy<T> getIterableWithPolicy(T[] elements) {
-        return null; // TODO: return the implementation of IterableWithPolicy
+        return new IterableWithPolicyImplementation<T>(elements);
     }
 
     public static void main(final String[] args) {
